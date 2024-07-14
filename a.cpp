662c17a29fca7e65b76b2476f4fe457e53398f1a
Mmian0125 <liyan939582@gmail.com>
2024-07-14 10:50:34 +0800

    配置环境

---
+++ a.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+void a(){
+    std::cout<<"this is a"<<std::endl;
+    int i=0;
+    while(i<10){
+        i++;
+    }   
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+void a();
+
+int main(){
+    cout<<"hi!"<<endl;
+    a();
+    return 0;
+}
