594b86871760b4bd990187bc247e07249ae47a57
Mmian0125 <liyan939582@gmail.com>
2024-07-23 21:38:06 +0800

    新增终端控制

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
+#include<thread>
+#include<chrono>
+#include"terminal.h"
 
-using namespace std;
-
-void a();
+using namespace std::chrono_literals;
 
 int main(){
-    cout<<"hi!"<<endl;
-    a();
+    /* tc::move_to(5,11);
+    tc::set_fore_color(214);
+    std::cout<<"hello world!";
+    tc::move_to(10,1);
+    tc::reset(); */
+    tc::hide_cursor();
+
+    int i=1;
+    while(true){
+        tc::clean_screen();
+        tc::move_to(i++,10);
+        tc::set_back_color(15);  //置为白色
+        std::cout<<"  ";
+        tc::reset();
+        std::cout<<std::flush;
+        std::this_thread::sleep_for(500ms);
+    }
     return 0;
 }
